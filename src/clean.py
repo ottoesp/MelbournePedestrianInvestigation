a418b9a442d273f47493570cd5a5166142fb18b3
ottoesp <otto.esperson@gmail.com>
2026-01-13 14:24:54 +1100

    Added further data cleaning, ready to transform

--- src/clean.py
+++ src/clean.py
@@ -1,7 +1,26 @@
 import pandas as pd
 
-def clean_sensor_locations(locations: pd.DataFrame) -> None:
+def clean_sensor_locations(locations: pd.DataFrame) -> pd.DataFrame:
     locations['installation_date'] = pd.to_datetime(
         locations['installation_date']
     )
+    locations = locations.drop(
+        columns=['note', 'direction_1', 'direction_2', 'latitude', 'longitude']
+    )
 
+    return locations
+
+def clean_sensor_counts(counts: pd.DataFrame) -> pd.DataFrame:
+    counts['sensing_date'] = pd.to_datetime(
+        counts['sensing_date']
+    )
+    counts = counts.drop(
+        # Dropping location as it is redundant with sensor_locations dataset
+        columns=['direction_1', 'direction_2', 'location']
+    )
+    counts = counts.rename(columns={
+        'total_of_directions' : 'count',
+        'id' : 'count_id'
+    })
+
+    return counts
--- src/load.py
+++ src/load.py
@@ -9,6 +9,7 @@ def load_sensor_locations() -> pd.DataFrame:
     sensor_locations_path =  DATA_DIR / 'pedestrian-counting-system-sensor-locations.parquet'
     if sensor_locations_path.exists():
         sensor_locations = pd.read_parquet(sensor_locations_path)
+
         return sensor_locations
     else:
         raise FileExistsError("Sensor Location Data does not exist")
@@ -17,6 +18,8 @@ def load_sensor_counts() -> pd.DataFrame:
     sensor_count_path =  DATA_DIR / 'pedestrian-counting-system-monthly-counts-per-hour.parquet'
     if sensor_count_path.exists():
         sensor_counts = pd.read_parquet(sensor_count_path)
+        
         return sensor_counts
     else:
         raise FileExistsError("Sensor Count Data does not exist")
+    
