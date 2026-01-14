8b41a5e46ca13d9512b06fa48750351f227b8ca4
ottoesp <otto.esperson@gmail.com>
2026-01-14 11:40:00 +1100

    Changed location_id to sensor_id

--- src/transform.py
+++ src/transform.py
@@ -4,11 +4,11 @@ from .config import FIRST_LOCKDOWN_START, LAST_LOCKDOWN_END
 def get_loc_counts(sensor_locations: pd.DataFrame, sensor_counts: pd.DataFrame) -> pd.DataFrame:
     loc_counts = pd.merge(
         sensor_locations, sensor_counts,
-        how='inner', on='location_id'
+        how='inner', on='sensor_id'
     )
     return loc_counts
 
 def categorise_as_pre_and_post_lockdowns(counts: pd.DataFrame) -> pd.DataFrame:
     # counts = counts[(counts['sensing_date_time'] < FIRST_LOCKDOWN_START) | (LAST_LOCKDOWN_END < counts['sensing_date_time'])]
-    counts = counts[(counts['sensing_date_time'] > FIRST_LOCKDOWN_START) & (LAST_LOCKDOWN_END > counts['sensing_date_time'])]
+    counts = counts[(counts['sensing_date_time'] < FIRST_LOCKDOWN_START) | (LAST_LOCKDOWN_END < counts['sensing_date_time'])]
     return counts
