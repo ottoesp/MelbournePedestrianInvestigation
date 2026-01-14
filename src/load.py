906a6fdd42d3c4f6c82a00cb0049e1282d954472
ottoesp <otto.esperson@gmail.com>
2026-01-14 11:40:22 +1100

    Added new pipeline for historic count data

--- src/load.py
+++ src/load.py
@@ -1,9 +1,5 @@
-from pathlib import Path
 import pandas as pd
-
-CURRENT_PATH = Path(__file__).resolve()
-PROJECT_ROOT = CURRENT_PATH.parent.parent
-RAW_DATA_DIR = PROJECT_ROOT / 'data' / 'raw'
+from .config import RAW_DATA_DIR, PROCESSED_DATA_FILE
 
 def load_sensor_locations() -> pd.DataFrame:
     sensor_locations_path =  RAW_DATA_DIR / 'pedestrian-counting-system-sensor-locations.parquet'
@@ -13,7 +9,19 @@ def load_sensor_locations() -> pd.DataFrame:
         return sensor_locations
     else:
         raise FileExistsError("Sensor Location Data does not exist")
-    
+
+def load_historic_sensor_counts(n_entries=-1) -> pd.DataFrame:
+    sensor_count_path =  RAW_DATA_DIR / 'Pedestrian_Counting_System_Monthly_counts_per_hour_may_2009_to_14_dec_2022.csv'
+    if sensor_count_path.exists():
+        if (n_entries >= 0):
+            sensor_counts = pd.read_csv(sensor_count_path, nrows=n_entries)
+        else:
+            sensor_counts = pd.read_csv(sensor_count_path)
+        
+        return sensor_counts
+    else:
+        raise FileExistsError("Sensor Count Data does not exist")
+
 def load_sensor_counts() -> pd.DataFrame:
     sensor_count_path =  RAW_DATA_DIR / 'pedestrian-counting-system-monthly-counts-per-hour.parquet'
     if sensor_count_path.exists():
@@ -23,3 +31,10 @@ def load_sensor_counts() -> pd.DataFrame:
     else:
         raise FileExistsError("Sensor Count Data does not exist")
     
+def load_processed() -> pd.DataFrame:
+    if PROCESSED_DATA_FILE.exists():
+        sensor_counts = pd.read_parquet(PROCESSED_DATA_FILE)
+        
+        return sensor_counts
+    else:
+        raise FileExistsError("Processed data does not exist")
