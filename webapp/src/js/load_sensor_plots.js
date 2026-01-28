5fb0a2a37e9646d0f779c8a5b6d2a3b7a2a8cace
ottoesp <otto.esperson@gmail.com>
2026-01-28 16:33:05 +1100

    Added sensor specific day changes

--- webapp/src/js/load_sensor_plots.js
+++ webapp/src/js/load_sensor_plots.js
@@ -1,18 +1,18 @@
 export async function loadSensorPlot(sensor) {
     const container = document.getElementById('sensor_plot_container');
     
-    const col = await createCountsPlot(sensor);
-    container.replaceChildren(col); // Single atomic operation, no flash
+    const sensor_plot = await createCountsPlot(sensor);
+    const days_plot = await createDaysPlot(sensor)
+    container.replaceChildren(sensor_plot, days_plot);
 }
 
 function createCountsPlot(sensor) {
     return new Promise((resolve) => {
         const col = document.createElement('div')
-        col.className = 'col-12'
+        col.className = 'col-12 col-lg-8';
 
         const card = document.createElement('div');
         card.className = 'card h-100';
-        card.style.minHeight = '400px';
         
         const img = document.createElement('img');
         img.src = `${import.meta.env.BASE_URL}sensorplots/${sensor.sensor_id}_sensor_plot.svg`;
@@ -20,11 +20,47 @@ function createCountsPlot(sensor) {
         img.alt = `Sensor plot for ${sensor.sensor_description}`;
         img.decoding = 'async';
         
+        const body = document.createElement('div');
+        body.className = 'card-body'
+
         const footer = document.createElement('div');
         footer.className = 'card-footer text-center';
         footer.innerHTML = '<p class="card-text text-secondary">Figure X</p>';
         
         card.appendChild(img);
+        card.appendChild(body);
+        card.appendChild(footer);
+        col.appendChild(card);
+        
+        // Resolve when image loads
+        img.onload = () => resolve(col);
+        img.onerror = () => resolve(col);
+    });
+}
+
+function createDaysPlot(sensor) {
+    return new Promise((resolve) => {
+        const col = document.createElement('div')
+        col.className = 'col-12 col-md-6 col-lg-4';
+
+        const card = document.createElement('div');
+        card.className = 'card h-100';
+        
+        const img = document.createElement('img');
+        img.src = `${import.meta.env.BASE_URL}sensordays/${sensor.sensor_id}_sensordays.svg`;
+        img.className = 'card-img-bottom img-fluid';
+        img.alt = `Days plot for ${sensor.sensor_description}`;
+        img.decoding = 'async';
+        
+        const body = document.createElement('div');
+        body.className = 'card-body'
+
+        const footer = document.createElement('div');
+        footer.className = 'card-footer text-center';
+        footer.innerHTML = '<p class="card-text text-secondary">Figure X</p>';
+        
+        card.appendChild(img);
+        card.appendChild(body);
         card.appendChild(footer);
         col.appendChild(card);
         
